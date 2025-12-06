4cc5a77b31fe9063382fae2cfee09fd333e2c20a
pierredeveloper <pierrejeanba1947@gmail.com>
2025-12-06 15:13:41 -0400

    push streamlit app code

--- chatbot.py
+++ chatbot.py
@@ -30,6 +30,8 @@ for message in st.session_state.chat_history:
 # Initialize LLM with HIGH variability
 llm = ChatGroq(
     #model="llama-3.3-70B-versatile",
+    #api_key=os.getenv("GROQ_API_KEY"),
+    api_key=st.secrets["GROQ_API_KEY"],
     model="llama-3.1-8b-instant",
     temperature=0.9,           # ← more creativity
     model_kwargs={"top_p": 0.9},                 # ← diverse word choices
