1afeab9cae34aff1b0e6f148332179f822d88d7d
pierrejeandata <pierrejeanba1947@gmail.com>
2026-09-15 09:57:19 -0400

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1552,9 +1552,9 @@ for msg in chat_history:
 # --------------------------------------------------
 llm = ChatGroq(
     api_key=os.getenv("OLLAMA_API_KEY"),
-    #model="openai/gpt-oss-20b",
+    model="openai/gpt-oss-20b",
     #model="whisper-large-v3",
-    model="openai/gpt-oss-120b",
+    #model="openai/gpt-oss-120b",
     temperature=0.7,
     model_kwargs={"top_p": 0.9}
 )
