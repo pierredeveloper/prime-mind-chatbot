4e745432633bd6834a93189cfc206d46afb3eeec
pierrejeandata <pierrejeanba1947@gmail.com>
2026-09-15 09:16:53 -0400

    Change model from llama-3.3-70b to whisper-large-v3

--- chatbot.py
+++ chatbot.py
@@ -1553,7 +1553,7 @@ for msg in chat_history:
 llm = ChatGroq(
     api_key=os.getenv("OLLAMA_API_KEY"),
     #model="openai/gpt-oss-20b",
-    model="llama-3.3-70b-versatile"
+    model="whisper-large-v3"
    #model="openai/gpt-oss-120b",
     temperature=0.7,
     model_kwargs={"top_p": 0.9}
